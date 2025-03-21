6c73d99a744421da0e804478514007be96034b02
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-21 12:19:51 +0530

    Adding cube program file

---
+++ src/4. cube.py
@@ -0,0 +1,9 @@
+"""
+WAP to save the cube of all numbers from 1 to a number n in list,
+where n is taken as input from the user.
+
+For e.g. Input: 5
+        Output: [1, 8, 27, 64, 125]  #Cube of numbers from 1 to 5 where 5 is the input from the user
+
+
+"""
