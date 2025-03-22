c55e34a4df620d8f0b57c7f73995aba805a19887
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-23 00:52:05 +0530

    Adding sum_of_series program file

---
+++ src/9. sum_of_series.py
@@ -0,0 +1,14 @@
+"""
+Write a program to calculate the sum of series up to n terms for a digit d.
+n & d are taken as input from the user.
+
+For example:
+    Input: n = 5, d = 2
+    Logic: 2 + 22 + 222 + 2222 + 22222
+    Output: 24690
+
+
+"""
+
+n: int= int(input("Enter the number of terms wanted: "))
+d: str= input("Enter the series wanted: ")
