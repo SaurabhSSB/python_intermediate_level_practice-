fb0744c5b3e79fe44ebf788aaa4e041270ac6c72
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-26 08:40:20 +0530

    Adding reverse program file

---
+++ src/e5. reverse.py
@@ -0,0 +1,11 @@
+"""
+WAP to reverse a given integer number.
+Example: Input: 105320
+        Output: 023501
+
+A program that prompts the user for an integer value and displays it's reverse value.
+"""
+
+value_1: str = input("Enter an integer: ")
+
+print(value_1[::-1])
