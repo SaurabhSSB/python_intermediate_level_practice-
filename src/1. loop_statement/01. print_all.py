47fce02adda789ad8de33ad27302986baeffe638
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-28 20:42:23 +0530

    Updating print_all program file

--- src/1. loop_statement/01. print_all.py
+++ src/1. loop_statement/01. print_all.py
@@ -2,7 +2,7 @@
 WAP to print all elements of a list using a for loop
 
 A program that prompts the user for elements of list and
-displays all the elements of the list.
+displays all the elements of the list in their respective data types.
 """
 
 n: int = int(input("Enter the size of list: "))
@@ -10,7 +10,9 @@ l: list[str] = []
 
 for i in range(n):
     x: str = input(f"Enter the {i+1}th element: ")
-    l.append(x)
+    if x.isdigit():
+        l.append(int(x))
+    else:
+        l.append(x)
 
-for j in l:
-    print(j)
+print(l)
